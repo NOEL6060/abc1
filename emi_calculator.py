c56b1d53e1d407809f634ab236206221694b397a
NOEL6060 <noelvictor24@gmail.com>
2026-06-19 20:45:43 +0530

    emi_calculator.py

--- emi_calculator.py
+++ emi_calculator.py
@@ -1,17 +1,17 @@
 def calculate_emi(principal, annual_rate, years):
 
 
-monthly_rate = annual_rate / 12 / 100
-months = years * 12
-
-emi = (
-    principal * monthly_rate *
-    (1 + monthly_rate) ** months
-) / (
-    ((1 + monthly_rate) ** months) - 1
-)
-
-return round(emi, 2)
+        monthly_rate = annual_rate / 12 / 100
+        months = years * 12
+        
+        emi = (
+            principal * monthly_rate *
+            (1 + monthly_rate) ** months
+        ) / (
+            ((1 + monthly_rate) ** months) - 1
+        )
+        
+        return round(emi, 2)
 
 
 loan_amount = float(input("Enter loan amount: "))
